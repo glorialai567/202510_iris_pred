8b8d8d23d8bad6b8b5a0a4d95cbd2073ed7e9dcb
glorialai567 <glorialai567@gmail.com>
2025-10-20 21:57:03 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+page=st.navigation([
+    st.Page('p1.py',title='IRIS相關資訊',icon='🌸'), #大寫Page是一個解析器
+    st.Page('p2.py',title='單一分類器展示',icon='🏵️'),
+    st.Page('p3.py',title='IRIS品種預測',icon='🧮'),
+])
+page.run()
